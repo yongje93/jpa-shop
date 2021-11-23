2ea34d459f567cfdb064f8343aa88b07ca5df9cb
yongje93 <yongje1211@gmail.com>
2021-11-23 23:18:06 +0900

    주문 조회 V3: 엔티티를 DTO로 변환 - 패치 조인 최적화 주문 조회 V3.1: 엔티티를 DTO로 변환 - 페이징 한계 돌파

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -9,6 +9,7 @@ import jpabook.jpashop.repository.OrderSearch;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
@@ -39,7 +40,7 @@ public class OrderApiController {
     }
 
     /**
-     * V2. 엔티티를 조회해서 DTO로 변환(fetch join 사용X)
+     * V2. 엔티티를 조회해서 DTO로 변환 (fetch join 사용X)
      * - 트랜잭션 안에서 지연 로딩 필요
      */
     @GetMapping("/api/v2/orders")
@@ -49,6 +50,33 @@ public class OrderApiController {
                 .map(o -> new OrderDto(o))
                 .collect(Collectors.toList());
 
+        return result;
+    }
+
+    /**
+     *  V3. 엔티티를 조회해서 DTO로 변환 (fetch join 사용O)
+     */
+    @GetMapping("/api/v3/orders")
+    public List<OrderDto> ordersV3() {
+        List<Order> orders = orderRepository.findAllWithItem();
+        List<OrderDto> result = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+        return result;
+    }
+
+    /**
+     * V3.1 엔티티를 조회해서 DTO로 변환 페이징 고려
+     * - ToOne 관계만 우선 모두 페치 조인으로 최적화
+     * - 컬렉션 관계는 hibernate.default_batch_fetch_size, @BatchSize로 최적화
+     */
+    @GetMapping("/api/v3.1/orders")
+    public List<OrderDto> ordersV3_page(@RequestParam(value = "offset",defaultValue = "0") int offset,
+                                        @RequestParam(value = "limit", defaultValue = "100") int limit) {
+        List<Order> orders = orderRepository.findAllWithMemberDelivery(offset, limit);
+        List<OrderDto> result = orders.stream()
+                    .map(o -> new OrderDto(o))
+                    .collect(Collectors.toList());
         return result;
     }
 
