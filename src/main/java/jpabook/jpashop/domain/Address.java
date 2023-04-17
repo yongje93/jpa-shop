e13bf017ff59ad77c914591201158f257d0657d0
Yongje Shin <yongje1211@gmail.com>
2023-04-17 22:20:44 +0900

    값 타입은 변경 불가능하게 설계

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -1,22 +1,21 @@
 package jpabook.jpashop.domain;
 
 import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+/**
+ * 주소 - 값 타입은 변경 불가능하게 설계
+ */
 @Embeddable
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class Address {
 
     private String city;
     private String street;
     private String zipcode;
-
-    protected Address() {
-    }
-
-    public Address(String city, String street, String zipcode) {
-        this.city = city;
-        this.street = street;
-        this.zipcode = zipcode;
-    }
 }
