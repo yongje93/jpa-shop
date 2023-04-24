9373f15627a17394bf30c342152ec5843534b5ef
Yongje Shin <yongje1211@gmail.com>
2023-04-24 22:44:58 +0900

    spring boot 3.0 update

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -1,6 +1,6 @@
 package jpabook.jpashop;
 
-import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+import com.fasterxml.jackson.datatype.hibernate5.jakarta.Hibernate5JakartaModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -13,7 +13,7 @@ public class JpashopApplication {
 	}
 
 	@Bean
-	Hibernate5Module hibernate5Module() {
-		return new Hibernate5Module();
+	Hibernate5JakartaModule hibernate5Module() {
+		return new Hibernate5JakartaModule();
 	}
 }
