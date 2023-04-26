55687ac9a9f15e8fe72b835932b8c451109485e8
yongje.shin <yongje1211@gmail.com>
2023-04-26 09:45:13 +0900

    P6Spy Format 설정

---
+++ src/main/java/jpabook/jpashop/config/P6SpyConfig.java
@@ -0,0 +1,13 @@
+package jpabook.jpashop.config;
+
+import com.p6spy.engine.spy.P6SpyOptions;
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class P6SpyConfig {
+    @PostConstruct
+    public void setLogMessageFormat() {
+        P6SpyOptions.getActiveInstance().setLogMessageFormat(P6SpySqlFormatter.class.getName());
+    }
+}
